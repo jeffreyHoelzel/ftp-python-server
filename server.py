96d376c53290a867169e1b250214f7e20e109953
jeffreyHoelzel <jmh2338@nau.edu>
2024-12-04 12:20:54 -0700

    Added function to handle finding an ftp room for a user to either create or join

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+################################################################################################################
+# ***IMPORTANT DISCLAIMER***
+# Much of the code used in this assignment was borrowed and/or modified from my Homework 2 assignment for CS460. 
+# Jeffrey Hoelzel Jr
+################################################################################################################
+
 import socket
 import sys
 import time
@@ -35,7 +41,50 @@ class FTPRoom:
         for client in self.clients:
             client.send(file) # file includes the entire path to file in host os
 
+# prompts a user to join an existing, or set up a new FTP room
+def ftp_room_prompt(ftp_rooms, client):
+    # request and receive username from client
+    client.send("USERNAME".encode("utf-8"))
+    username = client.recv(BUFFER_SIZE).decode("utf-8")
 
+    # send prompt message to user about setting up a new room or joining an existing one
+    client.send(f"Welcome to the FTP server!\nYou can either join an existing FTP room or create a new one.\nEnter the name of the FTP room you wish to join or enter 'NEW' to create a new FTP room.\n(If none are listed, create one).\n\nSend any files you want! To quit, enter 'CLOSE'".encode("utf-8"))
+
+    # display rooms if available
+    if len(ftp_rooms) != 0:
+        for room in ftp_rooms:
+            client.send(f"   {room}\n".encode("utf-8"))
+    else:
+        client.send("There are no rooms available to join. Create a new one!".encode("utf-8"))
+
+    # get client response
+    raw_choice = client.recv(BUFFER_SIZE).decode("utf-8")
+    cleaned_choice = clean_message(raw_choice) # remove any chat modifications to string
+
+    if "NEW" in cleaned_choice:
+        # request and receive room name
+        client.send("NEW".encode("utf-8"))
+        raw_room_name = client.recv(BUFFER_SIZE).decode("utf-8")
+        cleaned_room_name = clean_message(raw_room_name)
+
+        # return room name and username to caller
+        return cleaned_room_name, username
+    
+    # otherwise, figure out which room clients wants to join and return
+    for room in ftp_rooms:
+        if room.name == cleaned_choice:
+            return cleaned_choice, username
+        
+    # if each option fails, notify caller and return empty string
+    print(f"{username} failed to specify either a new room to create or an existing one.")
+    return "error", "error"
+
+
+
+
+
+def clean_message(message):
+    pass
 
 if __name__ == '__main__':
     print(f"FTP server booting up...\n\nTo get started, connect a client.\n")
