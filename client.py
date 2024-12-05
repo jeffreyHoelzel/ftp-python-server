87fe8105807220eb6f3d1f473ffe1edabd0bc842
jeffreyHoelzel <jmh2338@nau.edu>
2024-12-05 13:30:19 -0700

    Implemented first iteration of send and receive files

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket as s
 import threading as t
 import regex as re
+import os
 import sys
 
 # constants
@@ -9,6 +10,14 @@ PORT = 8080
 BUFFER_SIZE = 2048
 # regex pattern for matching filepaths
 FILE_PATH_PATTERN = r'^(.+/)*[^/]+\.[a-zA-Z0-9]+$'
+
+# remove excess characters used to specify file room chatting prompt in terminal
+def clean_message(message):
+    idx = message.find(">")
+    if idx != -1:
+        clean_message = message[idx + 3:]
+        return clean_message
+    return message
 
 def get_message():
     while True:
@@ -37,17 +46,65 @@ def send_message():
 
         # check if pattern matches a file path
         if re.match(FILE_PATH_PATTERN, message):
-            send_file(f"{username} >> {message}".encode("utf-8"))
+            send_file(message)
         client.send(f"{username} >> {message}".encode("utf-8"))
 
         if message == "CLOSE":
             sys.exit(0)
 
-def get_file(file_path):
-    pass
+def get_file(filename):
+    # parse out username
+    filename = clean_message(filename)
 
-def send_file(file_path):
-    pass
+    # parse out file and size
+    filename, file_size = filename.split(":")
+    file_size = int(file_size)
+
+    # send OK to other client
+    client.send(f"Received {filename}".encode("utf-8"))
+
+    # get path to save file
+    full_path = os.path.join(".", filename) # saving to current directory
+
+    # get all file contents
+    with open(full_path, "wb") as f:
+        bytes_received = 0
+        while bytes_received < file_size:
+            # read in chunks
+            chunk = client.recv(min(BUFFER_SIZE, file_size - bytes_received))
+            # no more chunks usually means done
+            if not chunk:
+                break
+            f.write(chunk)
+            bytes_received += len(chunk)
+
+    print(f"{filename} successfully received. Verify your current directory.")
+
+def send_file(filename):
+    # check if file exists
+    if not os.path.isfile(filename):
+        print(f"{filename} does not exits in this directory.")
+        return
+    
+    # get size of file
+    file_size = os.path.getsize(filename)
+
+    # send the filename and size
+    client.send(f"{username} >> {os.path.basename(filename)}:{file_size}".encode("utf-8"))
+
+    # get ACK
+    client.recv(BUFFER_SIZE)
+
+    # send contents of file
+    with open(filename, "rb") as f:
+        while True:
+            # get parts of file in chunks
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                break
+            client.sendall(bytes_read)
+
+    print(f"{filename} sent successfully.")
 
 if __name__ == "__main__":
     username = input("Enter your username: ")
